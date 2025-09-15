3b31aa67d84e895c256a2cbf55667ef997115571
arya <indosiar755@gmail.com>
2025-09-15 09:48:30 +0700

    Update deleto.php

--- deleto.php
+++ deleto.php
@@ -4,4 +4,4 @@ include 'koneksi.php';
 $id = $_GET['id'];
 mysqli_query($conn, "DELETE FROM user WHERE id='$id'");
 
-echo "<script>alert('Data berhasil dihapus'); window.location='tabel.php';</script>";
+echo "<script>alert('Data berhasil dihapus'); window.location='index.php';</script>";
